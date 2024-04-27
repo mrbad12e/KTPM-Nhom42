4ffba3da4d9473c5cd2bc681df8d465994e6f749
Nguyễn Trọng Nghĩa <117703705+mrbad12e@users.noreply.github.com>
2024-04-27 22:00:19 +0700

    Revert "Demo frontend 1"

--- front_end/src/Service/userService.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-const handleLoginApi = (userEmail,userPassword) => {
-    return axios.post('http://localhost:5000/student/login',{email: userEmail,password: userPassword});
-
-}
-
-export {handleLoginApi }
