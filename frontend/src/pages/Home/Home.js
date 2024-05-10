1f374a871a6c14980dd58a4ee495822fe4566059
unknown <luongquangcbvn@gmail.com>
2024-05-10 12:43:34 +0700

    save

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'; 
-import { Container , Row, Col } from 'react-bootstrap'; 
+import { Container , Row } from 'react-bootstrap'; 
 import axios from 'axios';
 import Sidebar_student from '../../components/Layouts/Sidebar/sidebarStudent'
 import avatar from '../../../assets/img/avatar.jpg';
@@ -7,50 +7,45 @@ import globalstyles from '../../CSSglobal.module.css';
 import styles from './Home.module.css';
 
 export const Home = () => {
-    const [studentInfo, setStudentInfo] = useState({id: '', programName: '', name: '', phone: '', birthday: '', gender: '', address: '' });
-    
+    const [student, setStudent] = useState(null);
+
     useEffect(() => {
-        const fetchStudentInfo = async () => {
+        const fetchStudent = async () => {
             try {
                 const email = localStorage.getItem('email');  
-                if (email) {
-                    const response = await axios.post('/student/profile', {email});
-                    const userInfo = response.data.userInfo;
-                    setStudentInfo(userInfo);
-                    console.log('Student info:', userInfo);
+                const response = await axios.get(`/student?email=${email}`); // Thêm 'email=' vào trước tham số email
+                const students = response.data.students;
+                if (students.length > 0) {
+                    setStudent(students[0]);
                 }
             } catch (error) {
                 console.error('Failed to fetch student info:', error);
             }
         };
-        fetchStudentInfo();
+        fetchStudent();
     }, []);
-
-    const renderInfoStudent = () => {
-        return (
-            <div className={styles.wrapper}>
-                <Row style={{marginTop: '50px'}}>
-                    <img src={avatar} alt="anh" className={styles.image_student}/>
-                    <div className={styles.container}>
-                        <p><strong>MSSV: </strong>{studentInfo.id}</p>
-                        <p><strong>Chuyên ngành: </strong>{studentInfo.programName}</p>
-                        <p><strong>Họ và tên: </strong>{studentInfo.name}</p>
-                        <p><strong>Số điện thoại: </strong>{studentInfo.phone}</p>
-                        <p><strong>Ngày sinh: </strong>{studentInfo.birthday}</p>
-                        <p><strong>Giới tính: </strong>{studentInfo.gender}</p>
-                        <p><strong>Địa chỉ: </strong>{studentInfo.address}</p>
-                    </div>
-                </Row>
-            </div>       
-        );
-    };
 
     return (
         <div>
            <Sidebar_student/>
            <Container fluid className={globalstyles['main-background']}>
                 <div className={globalstyles.title}>THÔNG TIN SINH VIÊN</div>
-                {renderInfoStudent()}  
+                {student && (
+                    <div className={styles.wrapper}>
+                        <Row style={{marginTop: '50px'}}>
+                            <img src={avatar} alt="anh" className={styles.image_student}/>
+                            <div className={styles.container}>
+                                <p><strong>MSSV: </strong>{student.id}</p>
+                                <p><strong>Chuyên ngành: </strong>{student.program_id}</p>
+                                <p><strong>Họ và tên: </strong>{`${student.first_name} ${student.last_name}`}</p>
+                                <p><strong>Số điện thoại: </strong>{student.phone}</p>
+                                <p><strong>Ngày sinh: </strong>{new Date(student.birthday).toLocaleDateString()}</p>
+                                <p><strong>Giới tính: </strong>{student.gender === 'F' ? 'Nữ' : 'Nam'}</p>
+                                <p><strong>Địa chỉ: </strong>{student.address}</p>
+                            </div>
+                        </Row>
+                    </div>
+                )}
             </Container>
         </div>
     );
