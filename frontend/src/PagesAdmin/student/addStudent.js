4d4f52e49390de3f401035db2cdba95c443f92c3
unknown <luongquangcbvn@gmail.com>
2024-05-12 15:50:17 +0700

    fix bug

--- frontend/src/PagesAdmin/student/addStudent.js
+++ frontend/src/PagesAdmin/student/addStudent.js
@@ -6,7 +6,7 @@ import axios from 'axios';
 
 export const AddStudent = () => {
     const [mssv, setMssv] = useState('12345678');                   // Đặt các giá trị mặc định để dễ kiểm thử
-    const [email, setEmail] = useState('gay@gmail.com');            // Kiểm thử xong sẽ xóa đi
+    const [email, setEmail] = useState('quang@gmail.com');          // Kiểm thử xong sẽ xóa đi
     const [program_id, setProgram_id] = useState('533402');
     const [first_name, setFirst_name] = useState('Luong');
     const [last_name, setLast_name] = useState('Quang');
@@ -64,23 +64,99 @@ export const AddStudent = () => {
             <Container fluid className={globalstyles['main-background']}>
                 <Row>
                     <Col className="text-center mb-3">
-                        <h4>Thêm sinh viên</h4>
+                        <div className={globalstyles['title']}>Thêm sinh viên</div>
                     </Col>
                 </Row>
                 <Row>
                     <Col>
                         <Form.Label>MSSV</Form.Label>
-                        <InputGroup className="mb-3">
+                        <InputGroup style={{ width: '100px' }}>
                             <Form.Control
-                                placeholder="ID"
+                                placeholder="8 số"
                                 aria-label="ID"
                                 aria-describedby="basic-addon1"
                                 value={mssv}
-                                onChange={(event) => setMssv(event.target.value)}
+                                onChange={(event) => {
+                                    const onlyNums = event.target.value.replace(/[^0-9]/g, '');
+                                    const limitedNums = onlyNums.slice(0, 8);
+                                    setMssv(limitedNums);
+                                }}
+                                type="text"
+                                maxLength={8} 
+                                pattern="[0-9]*" 
+                            />
+                        </InputGroup>
+                    </Col>
+                    <Col>
+                        <Form.Label>Chương trình đào tạo</Form.Label>
+                        <InputGroup style={{ width: '100px' }}>
+                            <Form.Control
+                                placeholder="Program_id"
+                                aria-label="Program_id"
+                                aria-describedby="Program_id"
+                                value={program_id}
+                                onChange={(event) => setProgram_id(event.target.value)}
                                 type="text"
                             />
                         </InputGroup>
                     </Col>
+                </Row>             
+                <Row>
+                    <Col xs="auto">
+                        <Form.Label>Họ</Form.Label>
+                        <InputGroup style={{ width: '150px' }}>
+                            <Form.Control
+                                placeholder="Ho"
+                                aria-label="Ho"
+                                aria-describedby="basic-addon1"
+                                value={first_name}
+                                onChange={(event) => setFirst_name(event.target.value)}
+                                type="text"
+                            />
+                        </InputGroup>
+                    </Col>
+                    <Col >
+                        <Form.Label>Tên</Form.Label>
+                        <InputGroup style={{ width: '150px' }}>
+                            <Form.Control
+                                placeholder="Ten"
+                                aria-label="Ten"
+                                aria-describedby="basic-addon1"
+                                value={last_name}
+                                onChange={(event) => setLast_name(event.target.value)}
+                                type="text"
+                            />
+                        </InputGroup>
+                    </Col>
+                    <Col xs="auto">
+                        {/* Nội dung cho cột giới tính */}
+                        <Form.Label>Giới tính</Form.Label>
+                        <InputGroup style={{ width: '70px' }}>
+                            <Form.Control
+                                as="select"
+                                aria-label="Giới tính"
+                                value={gender === 'M' ? 'Nam' : 'Nữ'} 
+                                onChange={(event) => setGender(event.target.value === 'Nam' ? 'M' : 'F')}
+                            >
+                                <option>Nam</option>
+                                <option>Nữ</option>
+                            </Form.Control>
+                        </InputGroup>
+                    </Col>
+                    <Col>
+                        <Form.Label>Ngày sinh</Form.Label>
+                        <InputGroup style={{ width: '150px' }}>
+                            <Form.Control 
+                                placeholder="DD/MM/YY"
+                                aria-describedby="basic-addon1"
+                                type="date" 
+                                value={DoB}
+                                onChange={(event) => setDoB(event.target.value)}
+                            />
+                        </InputGroup>
+                    </Col>
+                </Row>
+                <Row>
                     <Col>
                         <Form.Label>Email</Form.Label>
                         <InputGroup className="mb-3">
@@ -95,78 +171,8 @@ export const AddStudent = () => {
                         </InputGroup>
                     </Col>
                     <Col>
-                        <Form.Label>Chương trình đào tạo</Form.Label>
-                        <InputGroup className="mb-3">
-                            <Form.Control
-                                placeholder="Program_id"
-                                aria-label="Program_id"
-                                aria-describedby="Program_id"
-                                value={program_id}
-                                onChange={(event) => setProgram_id(event.target.value)}
-                                type="text"
-                            />
-                        </InputGroup>
-                    </Col>
-                </Row>             
-                <Row>
-                    <Col>
-                        <Form.Label>Họ</Form.Label>
-                        <InputGroup className="mb-3">
-                            <Form.Control
-                                placeholder="Ho"
-                                aria-label="Ho"
-                                aria-describedby="basic-addon1"
-                                value={first_name}
-                                onChange={(event) => setFirst_name(event.target.value)}
-                                type="text"
-                            />
-                        </InputGroup>
-                    </Col>
-                    <Col>
-                        <Form.Label>Tên</Form.Label>
-                        <InputGroup className="mb-3">
-                            <Form.Control
-                                placeholder="Ten"
-                                aria-label="Ten"
-                                aria-describedby="basic-addon1"
-                                value={last_name}
-                                onChange={(event) => setLast_name(event.target.value)}
-                                type="text"
-                            />
-                        </InputGroup>
-                    </Col>
-                    <Col>
-                    <Form.Label>Giới tính</Form.Label>
-                        <InputGroup className="mb-3">
-                            <Form.Control
-                                as="select"
-                                aria-label="Giới tính"
-                                value={gender}
-                                onChange={(event) => setGender(event.target.value === 'Nam' ? 'M' : 'F')}
-                            >
-                                <option>Nam</option>
-                                <option>Nữ</option>
-                            </Form.Control>
-                        </InputGroup>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col>
-                        <Form.Label>Ngày sinh</Form.Label>
-                        <InputGroup className="mb-3">
-                            <Form.Control
-                                placeholder="DD/MM/YY"
-                                aria-label="DD/MM/yy"
-                                aria-describedby="basic-addon1"
-                                type="date" 
-                                value={DoB}
-                                onChange={(event) => setDoB(event.target.value)}
-                            />
-                        </InputGroup>
-                    </Col>
-                    <Col>
                         <Form.Label>Số điện thoại</Form.Label>
-                        <InputGroup className="mb-3">
+                        <InputGroup style={{ width: '200px' }}>
                             <Form.Control
                                 placeholder="So dien thoai"
                                 aria-label="So dien thoai"
@@ -191,34 +197,36 @@ export const AddStudent = () => {
                         </InputGroup>
                     </Col>
                 </Row>
-                <Col>
-                    <Form.Label>Ngày nhập học</Form.Label>
-                    <InputGroup className="mb-3">
-                        <Form.Control
-                            placeholder="Ngày nhập học"
-                            aria-label="Ngày nhập học"
-                            aria-describedby="basic-addon1"
-                            type="date" 
-                            value={joinDate}
-                            onChange={(event) => setJoinDate(event.target.value)}
-                        />
-                    </InputGroup>
-                </Col>
-                <Col>
-                    <Form.Label>Trạng thái</Form.Label>
-                    <InputGroup className="mb-3">
-                        <Form.Control
-                            as="select"
-                            aria-label="Trạng thái"
-                            value={status}
-                            onChange={(event) => setGender(event.target.value === 'Học' ? 'true' : 'false')}
-                            type="text"
-                        >
-                            <option>Học</option>
-                            <option>Bỏ học</option>
-                        </Form.Control>
-                    </InputGroup>
-                </Col>
+                <Row>
+                    <Col>
+                        <Form.Label>Ngày nhập học</Form.Label>
+                        <InputGroup style={{ width: '150px' }}>
+                            <Form.Control
+                                placeholder="Ngày nhập học"
+                                aria-label="Ngày nhập học"
+                                aria-describedby="basic-addon1"
+                                type="date" 
+                                value={joinDate}
+                                onChange={(event) => setJoinDate(event.target.value)}
+                            />
+                        </InputGroup>
+                    </Col>
+                    <Col>
+                        <Form.Label>Trạng thái</Form.Label>
+                        <InputGroup style={{ width: '100px' }}>
+                            <Form.Control
+                                as="select"
+                                aria-label="Trạng thái"
+                                value={status === 'true' ? 'Học' : 'Bỏ học'} 
+                                onChange={(event) => setStatus(event.target.value === 'Học' ? 'true' : 'false')}
+                                type="text"
+                            >
+                                <option>Học</option>
+                                <option>Bỏ học</option>
+                            </Form.Control>
+                        </InputGroup>
+                    </Col>
+                </Row>
                 <Row className="justify-content-end mb-3">
                     <Col xs="auto">
                         <div className="d-inline-block me-3">
