cc17f29115c80b620cdd40273fee29bc294e5a5a
mrbad12e <ntnghia9a7@gmail.com>
2024-05-23 10:04:27 +0700

    lecturerController update

--- backend/controllers/User/lecturer.js
+++ backend/controllers/User/lecturer.js
@@ -1,6 +1,7 @@
 import Lecturer from '../../models/User/lecturer.js';
 import Class from '../../models/School/class.js';
 import UserControllers from './user.js';
+import client from '../../config/db.js';
 
 export default class LecturerController extends UserControllers{
     static async readStudents(req, res, next) {
@@ -16,8 +17,8 @@ export default class LecturerController extends UserControllers{
 
     static async viewClassAssigned(req, res, next) {
         try {
-            const semester = req.body.semester;
-            const query = 'CALL lecturer.self_view_class_assigned($1)';
+            const semester = req.query.semester;
+            const query = 'SELECT * FROM lecturer.self_view_class_assigned($1)';
             const { rows } = await client.query(query, [semester]);
             res.status(200).json({
                 classes: rows,
@@ -34,7 +35,7 @@ export default class LecturerController extends UserControllers{
             const query = 'SELECT * FROM lecturer.report_grade_distribution($1)';
             const { rows } = await client.query(query, [class_id]);
             res.status(200).json({
-                rows,
+                rows: rows,
                 message: 'Grade distribution fetched successfully',
             });
         } catch (error) {
